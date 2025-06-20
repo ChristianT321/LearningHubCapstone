2f25cc118a941f73c0cd1ae31c053170b7142e88
RyanMVan-Popering <ryan.vanpopering@edu.sait.ca>
2025-06-19 21:20:24 -0600

    Update page.tsx

--- app/test1/page.tsx
+++ app/test1/page.tsx
@@ -1,9 +1,10 @@
 //This was originally made for my last project in webdev 2. All questions/ design have been reworked for this project.
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import Image from 'next/image'
+import JSConfetti from 'js-confetti'
 
 const questions = [
   {
@@ -36,6 +37,20 @@ export default function Test1() {
     ([index, answer]) => answer === questions[Number(index)].answer
   ).length
 
+  const allAnswered = Object.keys(selectedAnswers).length === questions.length
+  const allAnsweredCorrectly = score === questions.length
+
+  useEffect(() => {
+    if (allAnsweredCorrectly && allAnswered) {
+      const jsConfetti = new JSConfetti()
+      jsConfetti.addConfetti({
+        emojis: ['🌈', '✨', '🐾', '🐻', '🦫', '🐺'],
+        emojiSize: 30,
+        confettiNumber: 60,
+      })
+    }
+  }, [allAnsweredCorrectly, allAnswered])
+
   const handleAnswer = (index: number, choice: string) => {
     if (selectedAnswers[index]) return
     setSelectedAnswers({ ...selectedAnswers, [index]: choice })
@@ -43,9 +58,6 @@ export default function Test1() {
 
   const handleReset = () => setSelectedAnswers({})
   const handleContinue = () => router.push('/module2')
-
-  const allAnswered = Object.keys(selectedAnswers).length === questions.length
-  const allAnsweredCorrectly = score === questions.length
 
   return (
     <main className="relative min-h-screen w-full flex flex-col items-center justify-start text-center overflow-y-auto p-4">
@@ -157,4 +169,3 @@ export default function Test1() {
     </main>
   )
 }
-
